7691f2e30d90541a22d4ac124fc852648108f5e1
tonyg <aj.gianno@gmail.com>
2021-04-20 14:56:00 -0500

    cards, not panels!

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,19 +1,16 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
   styleUrls: ['./favorite.component.css'],
 })
-export class FavoriteComponent implements OnInit {
+export class FavoriteComponent {
   // set alias for component selector to maintain desired case sensitivity
   // alias can keep exclusivity for reference class name
+  // keeps api stable
   @Input('isFavorite') isFavorite: boolean;
-  @Output() change = new EventEmitter();
-
-  constructor() {}
-
-  ngOnInit(): void {}
+  @Output('change') change = new EventEmitter();
 
   onClick(isFavorite) {
     this.isFavorite = !this.isFavorite;
