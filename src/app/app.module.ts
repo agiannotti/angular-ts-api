a7598ff62e634e3e0c2c0ff34836c8aeff335953
tonyg <aj.gianno@gmail.com>
2021-04-21 15:43:12 -0500

    new clean form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { PanelComponent } from './panel/panel.component';
 import { LikeComponent } from './like/like.component';
 import { ZippyComponent } from './zippy/zippy.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [AppComponent, PanelComponent, LikeComponent, ZippyComponent],
+  declarations: [AppComponent, PanelComponent, LikeComponent, ZippyComponent, ContactComponent],
   imports: [BrowserModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
