6ac7e0b214541571b4a80c097461bca863f6eea2
tonyg <aj.gianno@gmail.com>
2021-04-21 01:16:43 -0500

    project for tomorrow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PanelComponent } from './panel/panel.component';
 import { LikeComponent } from './like/like.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
-  declarations: [AppComponent, PanelComponent, LikeComponent],
+  declarations: [AppComponent, PanelComponent, LikeComponent, CartComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
